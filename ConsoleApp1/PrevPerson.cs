52f2f627dc57357d5029127b3a714bf55e21a43e
Ahmed Roberts <52471253+ahmedroberts@users.noreply.github.com>
2024-09-12 16:49:18 -0700

    Add project files.

---
+++ ConsoleApp1/PrevPerson.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaikageAhmed1
+{
+    enum OldGeneration { BabyBoomer, GenX, Millenial, GenZ, GenAlpha, GenBeta }
+    enum Generation { BabyBoomer, GenX, Millenial, GenZ, GenAlpha, GenBeta }
+
+    /*
+     * Realational Patterns Before & After 
+     */
+    internal class PrevPerson
+    {
+        // - Before
+        public required int BirthYear { get; set; }
+        public OldGeneration OldGeneration
+        {
+            get
+            {
+                if ((BirthYear >= 1946) && (BirthYear <= 1964))
+                {
+                    return OldGeneration.BabyBoomer;
+                }
+                else
+                {
+                    return OldGeneration.GenBeta;
+                }
+            }
+        }
+
+        // - After
+        public Generation Generation => BirthYear switch
+        {
+            (2000) => Generation.GenZ,
+            _ => Generation.GenBeta
+        };
+    }
+}
---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace RaikageAhmed1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var numbers = new int[] { 1,  2, 3, 4, 5 };
+            var sum = numbers.Sum();
+
+            Console.WriteLine("Hello, XRDS!");
+            Console.WriteLine(sum);
+
+            Console.WriteLine($"\n\nSo the sum was {sum} of {numbers}.\n\n");
+
+            /* Collection Initializers */
+            string[] oldStooges = new string[] { "Moe", "Larry", "Curly" };  // Old Way
+            
+            foreach (var s in oldStooges)
+            {
+                Console.WriteLine(s);
+            }
+
+            /* 
+             * C# 12
+             * .NET 8
+             * Collection Expressions &
+             * spread operators
+             * 
+             * Spread operator allows the collection to reference other collections
+             * and that all values from the referenced collections should be used to initalize
+             * that collection.
+             * 
+             */
+
+            string[] twoStooges  = ["Moe", "Larry"];
+            string[] moreStooges = ["Curly", "Shump"];
+            string[] fourStooges = [.. twoStooges, .. moreStooges]; // Collection Spread Operator
+
+        }
+    }
+}
